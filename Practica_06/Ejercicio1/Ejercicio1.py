436f06b106311ce8d4b59b97473898359e8776e9
PIkOOESP <miguelangel.s.b86@gmail.com>
2025-12-02 21:59:49 +0100

    Practica_06

--- Practica_06/Ejercicio1/Ejercicio1.py
+++ Practica_06/Ejercicio1/Ejercicio1.py
@@ -10,17 +10,17 @@ def menu():
     print("2. Mostrar todas las notas")
     print("3. Buscar una asignatura y mostrar su nota")
     print("4. Media de todas las notas")
-    
+    print("5. Salir")
     
 def anadirNota():
     asignatura = input("Asignatura: ").strip()
     nota = input("Nota: ").strip()
     with open("notas.txt", "a", encoding="utf-8") as fichero:
-        fichero.write(asignatura,"-",nota)
+        fichero.write(asignatura + "-" + nota + "\n")
     
 def mostrarNotas():
     with open("notas.txt", "r", encoding="utf-8") as fichero:
-        notas = fichero.readline()
+        notas = fichero.readlines()
         for n in notas:
             print(n)
             
@@ -28,17 +28,54 @@ def buscarAsignatura():
     asignatura = input("Asignatura: ").strip()
     
     with open("notas.txt", "r", encoding="utf-8") as fichero:
-        notas = fichero.readline()
+        notas = fichero.readlines()
         asignaturas = []
-        for n in notas:
-            asignaturas.push(n.split("-"))
+        for i in range(len(notas)):
+            asignaturas.append(notas[i].split("-"))
         
         for i in range(len(asignaturas)):
-            if(asignaturas[i] == asignatura):
-                print("Asignatura: ", asignaturas[i], "\nNota: ", asignaturas[i+1])
-                return
+            for a in range(len(asignaturas[i])):
+                if(asignaturas[i][a] == asignatura):
+                    print("Asignatura: ", asignaturas[i][a], "\nNota: ", asignaturas[i][a+1])
+                    return
             
         print("Asignatura no encontrada")
         
 def sumarNotas():
+    with open("notas.txt","r", encoding="utf-8") as fichero:
+        notas = fichero.readlines()
+        nota = []
+        total = 0
+        for n in notas:
+            nota.append(n.split("-"))
+            
+        for i in range(len(nota)):
+            for a in range(len(nota[i])):
+                try:
+                    total += float(nota[i][a])
+                except ValueError:
+                    None
         
+        print("Suma de todas las notas: ", total)
+
+
+
+# ----------- Programa principal -----------
+while True:
+    menu()
+
+    opcion = input("Opcion: ").strip()
+
+    if opcion == "1":
+        anadirNota()
+    elif opcion == "2":
+        mostrarNotas()
+    elif opcion == "3":
+        buscarAsignatura()
+    elif opcion == "4":
+        sumarNotas()
+    elif opcion == "5":
+        print("Saliendo...")
+        break
+    else:
+        print("Opcion no valida")
